e33ea9ed4190a8b6b25f35a8e89a16badbfb1882
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-04-14 20:00:03 +0000

    Create index.js

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    //instance d'authentification 
+    const backendApiNoAuth = axios.create({
+        baseURL: "http://localhost:8888/auth",
+        timeout: 80000,
+        headers: { "Content-Type": "application/json" },
+    });
+
+    //s'authentifier avec le Chef Labo LTI à changer (hejjaji pour labsip)
+    backendApiNoAuth.post(`/login`, { email: 'hassan.ouahmane@gmail.com', password: 'hassan.ouahmane' })
+        .then(function (response) {
+            const user = response.data
+            localStorage.setItem("user", JSON.stringify(response.data))
+
+            //instance de l'api apres l'authentification
+            const backendApi = axios.create({
+                baseURL: "http://localhost:8888/api",
+                timeout: 80000,
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + user.token,
+                },
+            });
+
+            //recuperation des utilisateurs du laboratoire
+            backendApi.get(`/labUsers/${user.laboratoriesHeaded[0]._id}`)
+                .then(function (response) {
+                    var images = $('#images');
+                    var op = "";
+                    response.data.forEach((user)=>{
+                        
+                        //s'il possede une image de profile
+                        if(user.profilePicture!=null || user.profilePicture!=undefined){
+                            op+= `<img src="http://localhost:8888/pictures/${user.profilePicture}" style="height:64px;width:64px"/>`
+                        }else{
+                            op+= `<img src="https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}" style="height:64px;width:64px"/>`
+                        }
+                    })
+                    images.html(op)
+                    
+                }).catch(function (error) {
+                    console.log(error)
+                })
+
+
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+})
