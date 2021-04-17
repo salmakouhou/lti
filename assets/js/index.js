94648bcce97fea792dce3cceeb455b9790b4b7fe
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-04-17 13:29:14 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,47 +27,68 @@ $(document).ready(function () {
                 .then(function (response) {
                     var chercheurs = $('#chercheursInfo');
                     var op = "";
-                    response.data.forEach((user)=>{
-                        
+                    response.data.forEach((user) => {
+
                         //s'il possede une image de profile
-                        if(user.profilePicture!=null || user.profilePicture!=undefined){
-                            op+='<div class="col-lg-6">'+
-                            '<div class="member d-flex align-items-start">'+
-                                `<div><img class="sp_img" src="https://app-rs-backend.herokuapp.com/pictures/${user.profilePicture}" alt=""></div>`+
-                                '<div class="member-info">'+
-                                    `<h4>Prof. ${user.firstName} ${user.lastName}</h4>`+
-                                    `<span></span>`+
-                                    '<p></p>'+
-                                    '<div class="social">'+
-                                        '<a href=""><i class="ri-twitter-fill"></i></a>'+
-                                        '<a href=""><i class="ri-facebook-fill"></i></a>'+
-                                        '<a href=""><i class="ri-instagram-fill"></i></a>'+
-                                       '<a href=""> <i class="ri-linkedin-box-fill"></i></a>'+
+                        if ((user.profilePicture != null || user.profilePicture != undefined)) {
+                            backendApi.get(`https://app-rs-backend.herokuapp.com/pictures/${user.profilePicture}`).then(function (response) {
+                                op += '<div class="col-lg-6">' +
+                                    '<div class="member d-flex align-items-start">' +
+                                    `<div><img class="sp_img" src="https://app-rs-backend.herokuapp.com/pictures/${user.profilePicture}" style="height:64px;width:64px" alt=""></div>` +
+                                    '<div class="member-info">' +
+                                    `<h4>Prof. ${user.firstName} ${user.lastName}</h4>` +
+                                    `<span></span>` +
+                                    '<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>' +
+                                    '<div class="social">' +
+                                    '<a href=""><i class="ri-twitter-fill"></i></a>' +
+                                    '<a href=""><i class="ri-facebook-fill"></i></a>' +
+                                    '<a href=""><i class="ri-instagram-fill"></i></a>' +
+                                    '<a href=""> <i class="ri-linkedin-box-fill"></i></a>' +
+                                    '</div>' +
+                                    '</div>' +
+                                    '</div>' +
+                                    '</div>'
+                            }).catch(function (error) {
+                                console.log(error)
+                                op+='<div class="col-lg-6">'+
+                                '<div class="member d-flex align-items-start">'+
+                                    `<div><img class="sp_img" src="https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}" style="height:64px;width:64px" alt=""></div>`+
+                                    '<div class="member-info">'+
+                                        `<h4>Prof. ${user.firstName} ${user.lastName}</h4>`+
+                                        `<span></span>`+
+                                        '<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>'+
+                                        '<div class="social">'+
+                                            '<a href=""><i class="ri-twitter-fill"></i></a>'+
+                                            '<a href=""><i class="ri-facebook-fill"></i></a>'+
+                                            '<a href=""><i class="ri-instagram-fill"></i></a>'+
+                                           '<a href=""> <i class="ri-linkedin-box-fill"></i></a>'+
+                                        '</div>'+
                                     '</div>'+
                                 '</div>'+
-                            '</div>'+
-                            '</div>'
-                        }else{
-                            op+='<div class="col-lg-6">'+
-                            '<div class="member d-flex align-items-start">'+
-                                `<div><img class="sp_img" src="https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}" alt=""></div>`+
-                                '<div class="member-info">'+
-                                    `<h4>Prof. ${user.firstName} ${user.lastName}</h4>`+
-                                    `<span></span>`+
-                                    '<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>'+
-                                    '<div class="social">'+
-                                        '<a href=""><i class="ri-twitter-fill"></i></a>'+
-                                        '<a href=""><i class="ri-facebook-fill"></i></a>'+
-                                        '<a href=""><i class="ri-instagram-fill"></i></a>'+
-                                       '<a href=""> <i class="ri-linkedin-box-fill"></i></a>'+
-                                    '</div>'+
-                                '</div>'+
-                            '</div>'+
-                            '</div>'
+                                '</div>'
+                            })
+
+                        } else {
+                            op += '<div class="col-lg-6">' +
+                                '<div class="member d-flex align-items-start">' +
+                                `<div><img class="sp_img" src="https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}" alt=""></div>` +
+                                '<div class="member-info">' +
+                                `<h4>Prof. ${user.firstName} ${user.lastName}</h4>` +
+                                `<span></span>` +
+                                '<p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>' +
+                                '<div class="social">' +
+                                '<a href=""><i class="ri-twitter-fill"></i></a>' +
+                                '<a href=""><i class="ri-facebook-fill"></i></a>' +
+                                '<a href=""><i class="ri-instagram-fill"></i></a>' +
+                                '<a href=""> <i class="ri-linkedin-box-fill"></i></a>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>'
                         }
                     })
                     chercheurs.html(op)
-                    
+
                 }).catch(function (error) {
                     console.log(error)
                 })
@@ -79,5 +100,5 @@ $(document).ready(function () {
         })
 
 
-        
+
 })
