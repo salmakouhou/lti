4f8a4348522d0e4d786875c539e2ecb73f0d3118
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-04-23 23:12:28 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,6 +56,8 @@ $(document).ready(function () {
                                    // userR[i] = user.roles;
 
                                 //}
+                                if (user.roles.includes("CED_HEAD")) return ["chef de CED"];
+                                if (user.roles.includes("VICE_CED_HEAD")) return ["Vice Président Chargé de la Recherche Scientifique"];
                                 op += '<div class="col-lg-6">' +
                                     '<div class="member d-flex align-items-start">' +
                                     `<div><img class="sp_img" src="http://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}" style="height:64px;width:64px" alt=""></div>` +
