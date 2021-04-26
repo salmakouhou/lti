476df64d210cf2c0169f2d612ae4714ebdd8e4f7
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-04-26 13:32:16 +0000

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
                             '<div class="member-info">' +
                             `<h3>${phdStudent.firstName} ${phdStudent.lastName}</h3>` +
                             `<span></span>` +
-                            `<h6><strong>Directeur de thèse :</strong>${convertSupervisor(phdStudent.supervisor)}</h6>` +
+                            `<h6><strong>Directeur de thèse :</strong>${user.supervisor}</h6>` +
                             `<h6><strong>Co-Directeur de thèse :</strong> ${user.coSupervisor}</h6>` +
                             `<h6><strong>Intitulé de la thèse :</strong> ${phdStudent.thesisTitle}</h6>` +
                             '</div>' +
