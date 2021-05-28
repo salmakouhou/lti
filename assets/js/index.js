77f4e23e16b9af2fe8d745f73d678a62e287fe1f
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-05-29 00:58:28 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -359,11 +359,11 @@ $(document).ready(function () {
                 })
 
             //
-            backendApi.get('/phdStudents/', { params: { "laboratory_id": "5f40f53095de870017abef56" } })
+            backendApi.get('/phdStudentsLabs', { params: { "laboratory_id": "5f40f53095de870017abef56" } })
                 .then(function (response) {
                     var phdStudentsCount = $('#phdStudentsCount');
                     var nbr = "" + response.data.length;
-                    phdStudentsCount.html(nbr - 13);
+                    phdStudentsCount.html(nbr);
 
 
 
